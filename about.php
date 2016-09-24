feab3a0dcf89dcbfdc34d2c23834bed1cbd222ab
Marc <marc@scrumdeveloper.nl>
2016-09-25 00:49:56 +0200

    first commit

---
+++ about.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Serious Games Portfolio</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/3-col-portfolio.css" rel="stylesheet">
+	<style>
+		.img-center {
+			margin: 0 auto;
+		}
+	</style>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">Serious Games</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="http://www.duckountancy.nl">Group Portfolio</a>
+                    </li>
+                    <li>
+						<a href="/assets/portfolio-mjs620.zip">Download deliverables (zip)</a>						
+                    </li>
+					
+                    <li>
+                        <a href="/about.php">About me</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+			<h1 class="page-header">
+			About Me
+			<small>Marc Jacobs (mjs620)</small>
+			</h1>
+			<p>Serious games is the last course of my master. At this moment, my work/study ratio is 50/50. Please find out more about my working career via <a href="https://nl.linkedin.com/in/scrumdeveloper">linked-in</a>. Below I summed up six topics that altogether define Marc Jacobs.</p>
+			</div>
+		</div>
+	
+		<!-- Team Members Row -->
+        <div class="row">
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="/img/1.jpg" alt="">
+                <h3>Software Developer</h3>
+                <p>My daily job is developing software in C#.NET</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="/img/2.jpg" alt="">
+                <h3>Army</h3>
+                <p>Previously, I served five years in the Dutch army in the signals department</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="/img/3.jpg" alt="">
+                <h3>The Hague</h3>
+                <p>Living in The Hague for 7 years, and I love this royal, political, international, but cosy city</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="/img/4.jpg" alt="">
+                <h3>Murakami</h3>
+                <p>Reading books is one of my hobbies, especially diving into the magical world of Haruki Murakami</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="/img/5.jpg" alt="">
+                <h3>Running</h3>
+                <p>Running is important to me, physical exercise clears the mind</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="/img/6.jpg" alt="">
+                <h3>Dreams</h3>
+                <p>In the near future I want to start as a freelancer and combine process, business, and technology</p>
+            </div>
+        </div>
+
+	<hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; mvjacobs.nl 2015</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Serious Games Portfolio</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/3-col-portfolio.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Serious Games</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="http://www.duckountancy.nl">Group Portfolio</a>
+                    </li>
+                    <li>
+						<a href="/assets/portfolio-mjs620.zip">Download deliverables (zip)</a>						
+                    </li>
+					
+                    <li>
+                        <a href="/about.php">About me</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="alert alert-warning alert-dismissible fade in text-center" role="warning">
+            <button class="close" aria-label="Close" data-dismiss="alert" type="button">
+                <span aria-hidden="true">×</span>
+            </button>
+            <p>If you want to read this portfolio in a more interactive way, please click the button below to play the portfolio game.</p>
+            <p>
+                <a href="/game/index.html" class="btn btn-warning" role="button">Play the game!</a>
+            </p>
+        </div>
+
+        <!-- Projects Row -->
+        <div class="row">
+            <div class="col-md-4 portfolio-item">
+                <a href="/assets/Project Utopia.pdf" target="_blank">
+                    <img class="img-responsive" src="/img/utopia.jpg" alt="">
+                </a>
+                <h3>
+                    <a href="/assets/Project Utopia.pdf" target="_blank">Project Utopia</a>
+                </h3>
+                <p>brief description of ideal society and potential role of serious games</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="/assets/moodboard.pdf" target="_blank">
+                    <img class="img-responsive" src="/img/ducktales.jpg" alt="">
+                </a>
+                <h3>
+                    <a href="/assets/moodboard.pdf" target="_blank">Moodspace</a>
+                </h3>
+                <p>results from the workshop <i>pitching</i>: moodspace of a serious game setting where ducktales and bookkeeping are combined</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="/assets/concept-presentation.pdf" target="_blank">
+                    <img class="img-responsive" src="/img/ducktales-game.png" alt="">
+                </a>
+                <h3>
+                    <a href="/assets/concept-presentation.pdf" target="_blank">Concept Application</a>
+                </h3>
+                <p>with elaboration of narrative(s) and visual design</p>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Projects Row -->
+        <div class="row">
+            <div class="col-md-4 portfolio-item">
+                <a href="http://www.duckountancy.nl">
+                    <img class="img-responsive" src="/img/ducksmoney.jpg" alt="">
+                </a>
+                <h3>
+                    <a href="http://www.duckountancy.nl">Final Application</a>
+                </h3>
+                <p>prototypes with sufficient documentation</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="/assets/Marc_essay.pdf" target="_blank">
+                    <img class="img-responsive" src="/img/ethics.jpg" alt="">
+                </a>
+                <h3>
+                    <a href="/assets/Marc_essay.pdf" target="_blank">Essay</a>
+                </h3>
+                <p>serious ethical games.. with serious fun</p>
+            </div>
+            <div class="col-md-4 portfolio-item">
+                <a href="/assets/Engagement-of-bird-lovers.pdf" target="_blank">
+                    <img class="img-responsive" src="/img/birds.jpg" alt="">
+                </a>
+                <h3>
+                    <a href="/assets/Engagement-of-bird-lovers.pdf" target="_blank">Workshop Engagement</a>
+                </h3>
+                <p>how to engage bird organizations?</p>
+            </div>
+        </div>
+
+	<hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; mvjacobs.nl 2015</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
